55032c038a97e93bbe4376283f7381f32010b82f
Rob Wells <rob@robjwells.com>
2023-12-07 11:06:17 +0000

    Day 3: rewrite using nom to parse

--- src/day_03.rs
+++ src/day_03.rs
@@ -1,261 +1,226 @@
-use std::{collections::HashSet, ops::Index, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::{Hash, Hasher},
+    ops::RangeInclusive,
+};
 
-const PUZZLE_INPUT: &str = include_str!("input/2023_03.txt");
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("input/2023_03.txt");
 
 pub fn run() -> String {
-    let grid: Grid = PUZZLE_INPUT.parse().expect("Failed to parse real input");
-    let part_one: u32 = grid.sum_of_part_numbers();
-    let part_two: u32 = grid.total_gear_ratio();
-    aoc_2023::both(part_one, part_two)
+    let grid = parse::parse_input(INPUT);
+    aoc_2023::both(grid.sum_of_part_numbers(), grid.total_gear_ratio())
 }
 
-#[derive(Debug)]
 struct Grid {
-    elements: Vec<Element>,
-    indices: Vec<Vec<usize>>,
-}
-
-impl Index<Point> for Grid {
-    type Output = Element;
-
-    fn index(&self, location: Point) -> &Self::Output {
-        let Point {
-            row: original_row,
-            column: original_column,
-        } = location;
-        let idx = self.indices[original_row][original_column];
-        &self.elements[idx]
-    }
+    map: HashMap<(usize, usize), Element>,
 }
 
 impl Grid {
-    fn rows_len(&self) -> usize {
-        self.indices.len()
-    }
-
-    fn cols_len(&self) -> usize {
-        self.indices[0].len()
-    }
-
-    fn symbols(&self) -> impl Iterator<Item = &Element> {
-        self.elements.iter().filter(|e| e.is_symbol())
-    }
-
-    fn symbols_with_part_numbers(&self) -> Vec<(&Element, Vec<&Element>)> {
-        let mut pairs = Vec::new();
-        for symbol in self.symbols() {
-            let unique_adjacents: HashSet<&Element> = symbol
-                .positions()
-                .into_iter()
-                .flat_map(|p| p.adjacents(self.rows_len(), self.cols_len()))
-                .map(|p| &self[p])
-                .filter(|e| e.is_number())
-                .collect();
-            pairs.push((symbol, unique_adjacents.into_iter().collect()));
-        }
-        pairs
+    fn at(&self, pos: (usize, usize)) -> Option<&Element> {
+        self.map.get(&pos)
     }
 
     fn sum_of_part_numbers(&self) -> u32 {
-        let unique_parts: HashSet<_> = self
-            .symbols_with_part_numbers()
-            .into_iter()
-            .flat_map(|(_sym, els)| els)
-            .collect();
-
-        unique_parts
-            .into_iter()
-            .filter_map(|el| {
-                if let Element::Number { value, .. } = el {
-                    Some(value)
-                } else {
-                    None
-                }
-            })
+        self.map
+            .values()
+            .filter(|el| el.kind.is_symbol())
+            .flat_map(|el| el.position.potential_adjacent_points())
+            .filter_map(|pos| self.at(pos))
+            .unique()
+            .filter_map(|el| el.kind.as_number())
             .sum()
     }
 
     fn total_gear_ratio(&self) -> u32 {
-        self.symbols_with_part_numbers()
-            .into_iter()
-            .filter(|(sym, els)| sym.is_gear() && els.len() == 2)
-            .map(|(_sym, els)| {
-                els.into_iter().filter_map(|el| {
-                    if let Element::Number { value, .. } = el {
-                        Some(value)
-                    } else {
-                        None
-                    }
-                })
+        self.map
+            .values()
+            .filter(|el| matches!(el.kind, ElementKind::Symbol('*')))
+            .filter_map(|el| {
+                let els = el
+                    .position
+                    .potential_adjacent_points()
+                    .filter_map(|pos| self.at(pos).filter(|el| el.kind.is_number()))
+                    .collect::<HashSet<_>>();
+                (els.len() == 2).then_some(els.into_iter())
             })
-            .map(|ns| ns.product::<u32>())
+            .map(|els| els.filter_map(|el| el.kind.as_number()).product::<u32>())
             .sum()
     }
 }
 
-#[derive(Debug, Hash, PartialEq, Eq)]
-enum Element {
-    Number { value: u32, positions: Vec<Point> },
-    Symbol { value: char, position: Point },
-    Blank { position: Point },
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Position {
+    row: usize,
+    column_start: usize,
+    column_end: usize,
+    offset: usize,
 }
 
-impl Element {
-    /// Returns `true` if the element is [`Number`].
+impl Position {
+    fn columns(&self) -> RangeInclusive<usize> {
+        self.column_start..=self.column_end
+    }
+
+    fn all_points(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        self.columns().map(|column| (self.row, column))
+    }
+
+    fn potential_adjacent_points(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        self.all_points().flat_map(|(row, col)| {
+            // Doesn't underflow in practice, faster than saturting sub.
+            [
+                (row + 1, col - 1),
+                (row + 1, col),
+                (row + 1, col + 1),
+                (row, col - 1),
+                (row, col + 1),
+                (row - 1, col - 1),
+                (row - 1, col),
+                (row - 1, col + 1),
+            ]
+            .into_iter()
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Element {
+    position: Position,
+    kind: ElementKind,
+}
+
+impl PartialEq for Element {
+    fn eq(&self, other: &Self) -> bool {
+        self.position.offset == other.position.offset
+    }
+}
+
+impl Eq for Element {}
+
+impl Hash for Element {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write_usize(self.position.offset)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum ElementKind {
+    Symbol(char),
+    Number(u32),
+}
+
+impl ElementKind {
+    /// Returns `true` if the element kind is [`Number`].
     ///
-    /// [`Number`]: Element::Number
+    /// [`Number`]: ElementKind::Number
     #[must_use]
     fn is_number(&self) -> bool {
-        matches!(self, Self::Number { .. })
+        matches!(self, Self::Number(..))
     }
 
-    fn positions(&self) -> Vec<Point> {
-        match self {
-            Element::Number { positions, .. } => positions.clone(),
-            Element::Symbol { position, .. } => [*position].into(),
-            Element::Blank { position } => [*position].into(),
-        }
-    }
-
-    /// Returns `true` if the element is [`Symbol`].
+    /// Returns `true` if the element kind is [`Symbol`].
     ///
-    /// [`Symbol`]: Element::Symbol
+    /// [`Symbol`]: ElementKind::Symbol
     #[must_use]
     fn is_symbol(&self) -> bool {
-        matches!(self, Self::Symbol { .. })
+        matches!(self, Self::Symbol(..))
     }
 
-    fn is_gear(&self) -> bool {
-        matches!(self, Element::Symbol { value: '*', .. })
-    }
-}
-
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-struct Point {
-    row: usize,
-    column: usize,
-}
-
-impl Point {
-    fn new(row: usize, column: usize) -> Self {
-        Self { row, column }
-    }
-
-    fn adjacents(&self, rows: usize, columns: usize) -> Vec<Self> {
-        HashSet::from([
-            (self.row.saturating_sub(1), self.column.saturating_sub(1)),
-            (self.row.saturating_sub(1), self.column),
-            (self.row.saturating_sub(1), self.column + 1),
-            (self.row, self.column.saturating_sub(1)),
-            (self.row, self.column + 1),
-            (self.row + 1, self.column.saturating_sub(1)),
-            (self.row + 1, self.column),
-            (self.row + 1, self.column + 1),
-        ])
-        .into_iter()
-        .map(|(row, col)| Point::new(row, col))
-        .filter(|p| p.row < rows && p.column < columns)
-        .collect()
-    }
-}
-
-impl PartialOrd for Point {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Point {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        if self.row == other.row {
-            self.column.cmp(&other.column)
+    fn as_number(&self) -> Option<&u32> {
+        if let Self::Number(v) = self {
+            Some(v)
         } else {
-            self.row.cmp(&other.row)
+            None
         }
     }
 }
 
-impl FromStr for Grid {
-    type Err = &'static str;
+mod parse {
+    use std::collections::HashMap;
 
-    #[allow(unused_variables)]
-    fn from_str(s: &str) -> Result<Grid, Self::Err> {
-        let lines: Vec<_> = s.lines().collect();
-        let width = lines[0].len();
-        let height = lines.len();
+    use super::{Element, ElementKind, Grid, Position};
+    use nom::{
+        branch::alt,
+        bytes::complete::is_a,
+        character::complete::{anychar, digit1, line_ending},
+        combinator::value,
+        multi::many1,
+        IResult,
+    };
+    use nom_locate::{position, LocatedSpan};
 
-        let mut out: Vec<Vec<Element>> = (0..height).map(|_| Vec::with_capacity(width)).collect();
+    type Span<'a> = LocatedSpan<&'a str>;
 
-        for (row, line) in lines.iter().enumerate() {
-            for (column, next) in line.chars().enumerate() {
-                let res = match next {
-                    '.' => Element::Blank {
-                        position: Point::new(row, column),
-                    },
-                    n if next.is_ascii_digit() => Element::Number {
-                        value: n.to_digit(10).unwrap(),
-                        positions: vec![Point::new(row, column)],
-                    },
-                    s if next.is_ascii_punctuation() => Element::Symbol {
-                        value: s,
-                        position: Point::new(row, column),
-                    },
-                    other => panic!("Unexpected character {:?}", other),
-                };
-                debug_assert!(out[row].len() == column);
-                out[row].push(res);
+    fn element_with_symbol(value: char, position: Span) -> Element {
+        let col = position.get_column() - 1;
+        let row = (position.location_line() - 1) as usize;
+        Element {
+            position: Position {
+                row,
+                column_start: col,
+                column_end: col,
+                offset: position.location_offset(),
+            },
+            kind: ElementKind::Symbol(value),
+        }
+    }
+
+    fn element_with_number(value: u32, length: usize, position: Span) -> Element {
+        let col = position.get_column() - 1;
+        let row = (position.location_line() - 1) as usize;
+        Element {
+            position: Position {
+                row,
+                column_start: col,
+                column_end: col + length - 1,
+                offset: position.location_offset(),
+            },
+            kind: ElementKind::Number(value),
+        }
+    }
+
+    pub fn parse_input(input: &str) -> Grid {
+        let (_remaining, results) = parse(input.into()).expect("Parsing failed.");
+        let mut map = HashMap::new();
+        for el in results.into_iter().flatten() {
+            for point in el.position.all_points() {
+                map.insert(point, el);
             }
         }
+        Grid { map }
+    }
 
-        let mut elements = Vec::with_capacity(height);
-        for row in out.into_iter() {
-            let mut column = row.into_iter().peekable();
-            while let Some(next) = column.next() {
-                if let Element::Number { value, positions } = next {
-                    let mut n = value;
-                    let mut pos = positions;
+    fn parse(input: Span) -> IResult<Span, Vec<Option<Element>>> {
+        many1(alt((
+            number,
+            value(None, is_a(".")),
+            value(None, line_ending),
+            symbol,
+        )))(input)
+    }
 
-                    while column.peek().is_some_and(Element::is_number) {
-                        let Some(Element::Number { value, positions }) = column.next() else {
-                            unreachable!()
-                        };
-                        n = n * 10 + value;
-                        pos.extend(positions);
-                    }
+    fn symbol(input: Span) -> IResult<Span, Option<Element>> {
+        let (remaining, position) = position(input)?;
+        let (remaining, symbol) = anychar(remaining)?;
+        Ok((remaining, Some(element_with_symbol(symbol, position))))
+    }
 
-                    pos.sort_unstable();
-                    elements.push(Element::Number {
-                        value: n,
-                        positions: pos,
-                    });
-                } else {
-                    elements.push(next);
-                }
-            }
-        }
-
-        let mut indices: Vec<Vec<usize>> = {
-            let mut v = Vec::with_capacity(height);
-            for _ in 0..height {
-                v.push(Vec::with_capacity(width));
-            }
-            v
-        };
-        for (element_idx, element) in elements.iter().enumerate() {
-            for Point { row, column } in element.positions() {
-                debug_assert!(indices[row].len() == column);
-                indices[row].push(element_idx);
-            }
-        }
-        Ok(Self { elements, indices })
+    fn number(input: Span) -> IResult<Span, Option<Element>> {
+        let (remaining, position) = position(input)?;
+        let (remaining, number) = digit1(remaining)?;
+        let length = number.len();
+        let value = number.parse().unwrap();
+        let res = element_with_number(value, length, position);
+        Ok((remaining, Some(res)))
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::day_03::PUZZLE_INPUT;
-
-    use super::{Element, Grid, Point};
+    use super::parse::parse_input;
+    use super::{Element, ElementKind, Grid, Position, INPUT as REAL_INPUT};
 
     const TEST_INPUT: &str = "\
         467..114..\n\
@@ -268,43 +233,61 @@ mod test {
         ......755.\n\
         ...$.*....\n\
         .664.598..";
-    #[test]
-    fn day3_parse_test_table() {
-        let grid: Grid = TEST_INPUT.parse().unwrap();
-        let Element::Symbol { value, position } = grid[Point::new(1, 3)] else {
-            panic!("Symbol not found at expected location.")
-        };
-        assert_eq!(value, '*');
-        assert_eq!(position, Point::new(1, 3));
 
-        let Element::Number { value, positions } = &grid[Point::new(4, 1)] else {
-            panic!("Number not found at expected location.")
-        };
-        assert_eq!(*value, 617);
+    #[test]
+    fn day3_parse_test_elements() {
+        let grid = parse_input(TEST_INPUT);
         assert_eq!(
-            positions,
-            &[Point::new(4, 0), Point::new(4, 1), Point::new(4, 2)]
+            grid.at((0, 0)),
+            Some(&Element {
+                position: Position {
+                    row: 0,
+                    column_start: 0,
+                    column_end: 2,
+                    offset: 0,
+                },
+                kind: ElementKind::Number(467)
+            })
+        );
+        assert_eq!(
+            grid.at((1, 3)),
+            Some(&Element {
+                position: Position {
+                    row: 1,
+                    column_start: 3,
+                    column_end: 3,
+                    offset: 14,
+                },
+                kind: ElementKind::Symbol('*')
+            })
         );
     }
 
     #[test]
     fn day3_sum_of_test_part_numbers() {
-        let grid: Grid = TEST_INPUT.parse().unwrap();
+        let grid: Grid = parse_input(TEST_INPUT);
         let sum: u32 = grid.sum_of_part_numbers();
         assert_eq!(sum, 4361);
     }
 
     #[test]
     fn day3_sum_of_real_part_numbers() {
-        let grid: Grid = PUZZLE_INPUT.parse().unwrap();
+        let grid: Grid = parse_input(REAL_INPUT);
         let sum: u32 = grid.sum_of_part_numbers();
         assert_eq!(sum, 535_235);
     }
 
     #[test]
     fn day3_test_total_gear_ratio() {
-        let grid: Grid = TEST_INPUT.parse().unwrap();
+        let grid: Grid = parse_input(TEST_INPUT);
         let gear_ratio: u32 = grid.total_gear_ratio();
         assert_eq!(gear_ratio, 467_835);
+    }
+
+    #[test]
+    fn day3_real_total_gear_ratio() {
+        let grid: Grid = parse_input(REAL_INPUT);
+        let gear_ratio: u32 = grid.total_gear_ratio();
+        assert_eq!(gear_ratio, 79_844_424);
     }
 }
